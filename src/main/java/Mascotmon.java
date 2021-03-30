8a0d5a947fb283ef3fd1fb204accca242a39a925
Scott Thomas <scottthomas@Scotts-Air-2.localdomain>
2021-03-29 22:37:27 -0400

    "Fix Defect#2; ID:#1 - Removed use of switch statement."

--- src/main/java/Mascotmon.java
+++ src/main/java/Mascotmon.java
@@ -5,7 +5,7 @@ public class Mascotmon {
     public String type;
     public Name name;
     public Stats stats;
-    public double weatherBonus = 1.0; 
+    public double weatherBonus = 1.0;
     public double typeBonus = 1.0;
     public int buf_counter = 0;
 
@@ -33,14 +33,14 @@ public class Mascotmon {
     }
 
     private void getType() {
-        type t = new type(name);
+        Type t = new Type(name);
         this.type = t.type;
     }
 
     private void getStats() {
         stats = new Stats(name);
-        
-        
+
+
     }
 
     private void getDescription() {
@@ -53,7 +53,7 @@ public class Mascotmon {
      * returns the base damage of the attack selected. The self-buff (attackNumber 0) can only be
      * used 3 times during a battle.
      * @return attack damage
-     * You can assume that this method uses the values it is supposed to use and is correct. 
+     * You can assume that this method uses the values it is supposed to use and is correct.
      */
     public Attack attack() {
         double attack_Damage = 0;
@@ -73,74 +73,72 @@ public class Mascotmon {
         String _desc= "";
         Attack attack = null;
 
-        switch (name) {
-            case ALBERT:
-                if (attackNumber == 0) {
-                    _desc = " uses Iron Scales, increasing defense stat by 10%";
-                    stats.defense *= 1.10;
-                    attack = new Attack(0, "None");
-                } else if (attackNumber == 1) {
-                    _desc = " uses Death Roll";
-                    attack = new Attack(stats.attack, "Ground");
-                } else if (attackNumber == 2) {
-                    _desc = " uses Chomp";
-                    attack = new Attack(stats.attack, "Normal");
-                } else {
-                    _desc = " uses Aqua Cannon";
-                    attack = new Attack(stats.attack, "Water");
-                }
-                break;
-            case RALPHIE:
-                if (attackNumber == 0) {
-                    _desc = " uses Iron Hide, increasing defense stat by 10%";
-                    stats.defense *= 1.10;
-                    attack = new Attack(0, "None");
-                } else if (attackNumber == 1) {
-                    _desc = " uses Ground Stomp";
-                    attack = new Attack(stats.attack, "Ground");
-                } else if (attackNumber == 2) {
-                    _desc = " uses Headbutt";
-                    attack = new Attack(stats.attack, "Normal");
-                } else {
-                    _desc = " uses Flaming Horn";
-                    attack = new Attack(stats.attack, "Fire");
-                }
-                break;
-            case SPARKY:
-                if (attackNumber == 0) {
-                    _desc = " uses Heat Up, increasing attack stat by 10%";
-                    stats.attack *= 1.10;
-                    attack = new Attack(0, "None");
-                } else if (attackNumber == 1) {
-                    _desc = " uses Inferno";
-                    attack = new Attack(stats.attack, "Fire");
-                } else if (attackNumber == 2) {
-                    _desc = " uses Quick Attack";
-                    attack = new Attack(stats.attack, "Normal");
-                    System.out.println("Attack value: " + stats.attack);
-                } else {
-                    _desc = " uses Earthquake";
-                    attack = new Attack(stats.attack, "Ground");
-                }
-                break;
-            case BULLY:
-                if (attackNumber == 0) {
-                    _desc = " uses Sleep, increasing health stat by 10%";
-                    double health = stats.health * 1.10;
-                    stats.health = Math.round(health);
-                    attack = new Attack(0, "None");
-                } else if (attackNumber == 1) {
-                    _desc = " uses Body Slam";
-                    attack = new Attack(stats.attack, "Normal");
-                } else if (attackNumber == 2) {
-                    _desc = " uses Splash";
-                    attack = new Attack(stats.attack, "Water");
-                } else {
-                    _desc = " uses Ground Pound";
-                    attack = new Attack(stats.attack, "Ground");
-                }
+
+
+        if (name.equals(Mascotmon.Name.ALBERT)) {
+            if (attackNumber == 0) {
+                _desc = " uses Iron Scales, increasing defense stat by 10%";
+                stats.defense *= 1.10;
+                attack = new Attack(0, "None");
+            } else if (attackNumber == 1) {
+                _desc = " uses Death Roll";
+                attack = new Attack(stats.attack, "Ground");
+            } else if (attackNumber == 2) {
+                _desc = " uses Chomp";
+                attack = new Attack(stats.attack, "Normal");
+            } else {
+                _desc = " uses Aqua Cannon";
+                attack = new Attack(stats.attack, "Water");
+            }
+        } else if (name.equals(Mascotmon.Name.RALPHIE)) {
+            if (attackNumber == 0) {
+                _desc = " uses Iron Hide, increasing defense stat by 10%";
+                stats.defense *= 1.10;
+                attack = new Attack(0, "None");
+            } else if (attackNumber == 1) {
+                _desc = " uses Ground Stomp";
+                attack = new Attack(stats.attack, "Ground");
+            } else if (attackNumber == 2) {
+                _desc = " uses Headbutt";
+                attack = new Attack(stats.attack, "Normal");
+            } else {
+                _desc = " uses Flaming Horn";
+                attack = new Attack(stats.attack, "Fire");
+            }
+        } else if (name.equals(Mascotmon.Name.SPARKY)) {
+            if (attackNumber == 0) {
+                _desc = " uses Heat Up, increasing attack stat by 10%";
+                stats.attack *= 1.10;
+                attack = new Attack(0, "None");
+            } else if (attackNumber == 1) {
+                _desc = " uses Inferno";
+                attack = new Attack(stats.attack, "Fire");
+            } else if (attackNumber == 2) {
+                _desc = " uses Quick Attack";
+                attack = new Attack(stats.attack, "Normal");
+                System.out.println("Attack value: " + stats.attack);
+            } else {
+                _desc = " uses Earthquake";
+                attack = new Attack(stats.attack, "Ground");
+            }
+        } else if (name.equals(Mascotmon.Name.BULLY)) {
+            if (attackNumber == 0) {
+                _desc = " uses Sleep, increasing health stat by 10%";
+                double health = stats.health * 1.10;
+                stats.health = Math.round(health);
+                attack = new Attack(0, "None");
+            } else if (attackNumber == 1) {
+                _desc = " uses Body Slam";
+                attack = new Attack(stats.attack, "Normal");
+            } else if (attackNumber == 2) {
+                _desc = " uses Splash";
+                attack = new Attack(stats.attack, "Water");
+            } else {
+                _desc = " uses Ground Pound";
+                attack = new Attack(stats.attack, "Ground");
+            }
         }
-            
+
         System.out.println(name.toString().toLowerCase() + _desc);
         return attack;
     }
