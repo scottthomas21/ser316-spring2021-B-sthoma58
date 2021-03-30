bd0bef3cb5adc76c023e5bd25b6448a35f6d8708
Scott Thomas <scottthomas@Scotts-Air-2.localdomain>
2021-03-30 01:15:56 -0400

    "deleted blackbox test"

--- src/test/java/caclulateDamageTest.java
+++ src/test/java/caclulateDamageTest.java
@@ -1,52 +1,105 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * 
- */
+//import main.java.BearWorkshop;
 
-/**
- * @author scottthomas
- *
- */
-class caclulateDamageTest {
+import static org.junit.Assert.*;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
+@RunWith(Parameterized.class)
+public class caclulateDamageTest {
+    private Class<BattleScenario> classUnderTest;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    @SuppressWarnings("unchecked")
+    public caclulateDamageTest(Object classUnderTest) {
+        this.classUnderTest = (Class<BattleScenario>) classUnderTest;
+    }
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+    @Parameters
+    public static Collection<Object[]> courseGradesUnderTest() {
+        Object[][] classes = {
+                {BattleScenario1.class},
+                {BattleScenario2.class},
+                {BattleScenario3.class},
+                {BattleScenario4.class},
+                {BattleScenario5.class}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+        };
+        return Arrays.asList(classes);
+    }
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    private BattleScenario createBattleScenario(Mascotmon a, Mascotmon d) throws Exception {
+        Constructor<BattleScenario> constructor = classUnderTest.getConstructor(Mascotmon.class, Mascotmon.class);
+        System.out.println(constructor);
+        return constructor.newInstance(a, d);
+    }
 
+    
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    // sample test
+
+    /**
+     * Battle between Bully and Ralphie on a sunny day with ground attack
+     * @throws Exception 
+     */
+    @Test
+    public void BvsRSunnyGround() throws Exception {
+        
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.BULLY);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+        
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
+        System.out.println("    BvsRSunnyGround");
+        
+        fight1.setEnvironment(Environment.Weather.sunny);
+        Attack attack = new Attack(80, "Ground");
+        
+        //Calculation: 80 * 1 * 1 - 65 * 1 *1
+        // 80 put into attack manually, no weather bonuses on either side, Ralphi has 65 defense
+        
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 15, 0.2);
+    }
+    
+    /**
+     * Battle between Bully and Ralphie on a sunny day with ground attack
+     * @throws Exception 
+     */
+    @Test
+    public void RvsBSunnyGround() throws Exception {
+        
+        Mascotmon attacker1 = new Mascotmon(Mascotmon.Name.RALPHIE);
+        Mascotmon defender1 = new Mascotmon(Mascotmon.Name.BULLY);
+        
+        BattleScenario fight1 = createBattleScenario(attacker1, defender1); 
+        System.out.println("    RvsBSunnyGround");
+        
+        fight1.setEnvironment(Environment.Weather.sunny);
+        Attack attack = new Attack(80, "Ground");
+        
+        //Calculation: 80 * 1.2 * 1 * 1 - 40 * 1 *1
+        // 80 put into attack manually, Ralhpie gets bonus for ground attack no weather bonuses or type bonus
+        
+        double damage = fight1.calculateDamage(attack, attacker1, defender1);
+        System.out.println("         Damage dealt: " + damage);
+        assertEquals(damage, 56, 0.2);
+    }
 }
